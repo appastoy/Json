fa7423f8852249b4e5c44602314cdcdc31169d57
appastoy <appastoy82@gmail.com>
2023-01-25 03:30:03 +0900

    New Json Document System Start

---
+++ AppAsToy.Json/Documents/JDocument.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppAsToy.Json.Documents;
+
+/// <summary>
+/// Json Document
+/// </summary>
+public sealed class JDocument
+{
+}
